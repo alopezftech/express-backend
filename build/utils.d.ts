3daac3364704fef4d80c877e751b7ab0a5589b1e
alopezf <u4062984656@gmail.com>
2025-08-27 08:52:38 +0100

    Eliminar build  del repositorio y actualizar .gitignore

--- build/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NewDiaryEntry } from "./types.d.ts";
-declare const toNewDiaryEntry: (object: any) => NewDiaryEntry;
-export default toNewDiaryEntry;
-//# sourceMappingURL=utils.d.ts.map
--- build/validations.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NewDiaryEntry } from "./models/types";
-declare const toNewDiaryEntry: (object: any) => NewDiaryEntry;
-export default toNewDiaryEntry;
-//# sourceMappingURL=validations.d.ts.map
